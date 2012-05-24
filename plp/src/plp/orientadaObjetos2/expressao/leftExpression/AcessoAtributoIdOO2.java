98cd85bf475049fa5a1d47336ba4fc9ab48002a3
Francisco Soares <chicaoiskemia@gmail.com>
2012-05-24 20:13:50 -0300

    Adicionado código base do projeto;

---
+++ plp/src/plp/orientadaObjetos2/expressao/leftExpression/AcessoAtributoIdOO2.java
@@ -0,0 +1,63 @@
+package plp.orientadaObjetos2.expressao.leftExpression;
+
+import plp.expressions2.memory.VariavelNaoDeclaradaException;
+import plp.orientadaObjetos1.excecao.declaracao.ClasseNaoDeclaradaException;
+import plp.orientadaObjetos1.expressao.leftExpression.AcessoAtributoId;
+import plp.orientadaObjetos1.expressao.leftExpression.Id;
+import plp.orientadaObjetos1.expressao.leftExpression.LeftExpression;
+import plp.orientadaObjetos1.memoria.AmbienteCompilacaoOO1;
+import plp.orientadaObjetos1.util.Tipo;
+import plp.orientadaObjetos2.memoria.DefClasseOO2;
+
+public class AcessoAtributoIdOO2 extends AcessoAtributoId{
+
+	public AcessoAtributoIdOO2(LeftExpression av, Id id) {
+		super(av, id);
+	}
+	
+	private boolean checaTipoClasseMae (AmbienteCompilacaoOO1 ambiente, Id idClasseMae) throws ClasseNaoDeclaradaException {
+		boolean retorno = false;
+		DefClasseOO2 defSuperClasse = (DefClasseOO2) ambiente.getDefClasse(idClasseMae);
+		
+		try {
+			defSuperClasse.getTipoAtributo(super.getId());
+			retorno = true;
+		} catch (VariavelNaoDeclaradaException atrib) {
+			if (defSuperClasse.getNomeSuperClasse() != null) {
+				retorno = this.checaTipoClasseMae(ambiente, defSuperClasse.getNomeSuperClasse());	
+			}
+   	 	}
+   	 		
+		return retorno;
+	}	
+	
+	@Override
+	public boolean checaTipo(AmbienteCompilacaoOO1 ambiente) throws VariavelNaoDeclaradaException, ClasseNaoDeclaradaException {
+        boolean resposta = false;
+        if(av.checaTipo(ambiente)) {
+            try{
+                Tipo tipo = av.getTipo(ambiente);
+                DefClasseOO2 defClasse = (DefClasseOO2) ambiente.getDefClasse(tipo.getTipo());
+                
+                if (defClasse.getNomeSuperClasse() != null) {
+                	resposta = this.checaTipoClasseMae(ambiente, defClasse.getNomeSuperClasse());
+                }
+                
+                // Se a verificacao pelo atributo nas classes maes nao encontrou nada
+                if (!resposta) {
+                    defClasse.getTipoAtributo(super.getId());
+                    resposta = true;  
+                }                
+            }
+            catch(VariavelNaoDeclaradaException atrib){
+                resposta = false;
+            }
+            catch(ClasseNaoDeclaradaException clas){
+                resposta = false;
+            }
+
+        }
+        return resposta;
+	}
+
+}
