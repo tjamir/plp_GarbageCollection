360e7652d7e71303fc325fadba610bb6a20ea3b3
tjamir <tjamir@gmail.com>
2012-05-27 01:11:57 -0300

    Adicionando programa de teste (não funcionando ainda)

--- plp/src/plp/orientadaObjetos1/memoria/ContextoExecucaoOO1.java
+++ plp/src/plp/orientadaObjetos1/memoria/ContextoExecucaoOO1.java
@@ -119,6 +119,8 @@ public class ContextoExecucaoOO1 implements AmbienteExecucaoOO1 {
         
         this.entrada = entrada;
         this.saida = new ListaValor();
+        
+        this.garbageColector=new SimpleMarkSweepGC();
     }
 
     /**
